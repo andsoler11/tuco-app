31edc753a5b00ffc58f408420a227d8f3b704189
Andres Soler <andres.soler@talent.com>
2022-06-09 20:05:23 -0500

    changed urls

--- tuco/urls.py
+++ tuco/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('apps.users.urls')),
-    path('dishes/', include('apps.dishes.urls')),
+    path('', include('apps.dishes.urls')),
 ]
