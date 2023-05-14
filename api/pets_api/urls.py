efd164880786d7c09a6ad07820da8343f9ee95d9
Andres Soler <andres.soler@talent.com>
2023-05-14 18:44:49 -0500

    added more test for new api endpoints

--- api/pets_api/urls.py
+++ api/pets_api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PetListView.as_view(), name='pet-list-api'),
-    path('<int:pk>/', views.PetDetailView.as_view(), name='pet-detail-api'),
+    path('detail/<str:pk>/', views.PetDetailView.as_view(), name='pet-detail-api'),
     path('create/', views.PetCreateView.as_view(), name='pet-create-api'),
+    path('menus/', views.MenusView.as_view(), name='menus-api'),
 ]
